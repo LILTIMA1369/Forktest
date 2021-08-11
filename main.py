f5baa52b58fd3f90074aa41079c9ebfedf72f0e8
DotVR46 <=>
2021-08-11 18:38:41 +0600

    Добавил генератор логина

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ import random
 
 def pass_gen(length=15):
 	keylist = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+	login = []
+
+	while len(login) < length:
+		char = random.choice(keylist)
+		login.append(char)
+
+	return ''.join(login)
+
+print('Ваш логин:', pass_gen())
+
+def pass_gen2(length=15):
+	keylist2 = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
 	password = []
 
 	while len(password) < length:
-		char = random.choice(keylist)
+		char = random.choice(keylist2)
 		password.append(char)
 
 	return ''.join(password)
 
-print(pass_gen())
+print('Ваш пароль:', pass_gen2())
