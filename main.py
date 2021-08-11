d36dad8e30df4c04536126500526d55b0c4f59ef
тима <tim_hz@Mail.ru>
2021-08-11 19:27:53 +0600

    Генератор каждые 5 сек

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
+import random 
+import time
 
 def pass_gen(length=15):
 	keylist = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
@@ -8,6 +9,16 @@ def pass_gen(length=15):
 		char = random.choice(keylist)
 		password.append(char)
 
-	return ''.join(password)
+	print(''.join(password))
 
 print(pass_gen())
+
+while True:
+	pass_gen()
+	time.sleep(5)
+
+# from crontab import CronTab
+# cron = CronTab(user='User')
+# job = cron.new(command='pass_gen')
+# job.minute.every(1)
+# cron.write()
